6ee26d7bde82a38d102e12b62840654abfb9027d
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2025-07-24 16:36:06 +0800

    Update readme file and bump version to 1.1.0

--- chip-for-tour-master.php
+++ chip-for-tour-master.php
@@ -3,7 +3,7 @@
  * Plugin Name: CHIP for Tour Master
  * Plugin URI: https://github.com/CHIPAsia/chip-for-tour-master
  * Description: Integrate CHIP with Tour Master
- * Version: 1.0.1
+ * Version: 1.1.0
  * Author: Chip In Sdn Bhd
  * Author URI: https://chip-in.asia
  * Requires at least: 6.1
@@ -37,7 +37,7 @@ function ctm_load() {
  * @since 1.0.0
  */
 function define_ctm_static() {
-	define( 'CTM_MODULE_VERSION', 'v1.0.1' );
+	define( 'CTM_MODULE_VERSION', 'v1.1.0' );
 	define( 'CTM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 	define( 'CTM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 }
